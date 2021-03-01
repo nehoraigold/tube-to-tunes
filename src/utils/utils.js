7de21ef8b9d5c01fc5fc6894a6a4ec0c60dc58a2
Ori Gold <nehoraigold@gmail.com>
2021-03-01 21:12:38 +0200

    utils: expanded utils to include json, path, and video id functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,13 @@
+//region imports
+const { readFileSync, existsSync } = require("fs");
+const { join, dirname } = require("path");
+//endregion
+
 const getQueryParamsString = (url) => {
     const QUERY_PARAMS_INDEX = 1;
     const QUERY_PARAMS_DELIMITER = "?";
     return url.split(QUERY_PARAMS_DELIMITER)[QUERY_PARAMS_INDEX];
-}
+};
 
 const stringToMap = (string, pairDelimiter, keyValueDelimiter) => {
     const KEY_INDEX = 0;
@@ -13,11 +18,35 @@ const stringToMap = (string, pairDelimiter, keyValueDelimiter) => {
     keyValuePairs.forEach(pair => {
         const keyVal = pair.split(keyValueDelimiter);
         map[keyVal[KEY_INDEX]] = keyVal[VALUE_INDEX];
-    })
+    });
     return map;
+};
+
+const getYoutubeVideoIdFromUrl = (url) => {
+    const VIDEO_ID_KEY = "v";
+    const PAIR_DELIMITER = "&";
+    const KEY_VALUE_DELIMITER = "=";
+    const queryParams = getQueryParamsString(url);
+    const params = stringToMap(queryParams, PAIR_DELIMITER, KEY_VALUE_DELIMITER);
+    return params[VIDEO_ID_KEY];
+};
+
+const getAbsolutePath = (pathFromRoot) => {
+    return join(dirname(require.main.filename), "..", pathFromRoot);
+};
+
+const getJsonFromFile = (filepath) => {
+    if (!existsSync(filepath)) {
+        throw `No JSON file at ${filepath}!`;
+    }
+    const jsonFile = readFileSync(filepath);
+    return JSON.parse(jsonFile.toString());
 };
 
 module.exports = {
     getQueryParamsString,
-    stringToMap
-}
+    stringToMap,
+    getYoutubeVideoIdFromUrl,
+    getAbsolutePath,
+    getJsonFromFile
+};
