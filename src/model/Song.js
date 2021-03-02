5093f3ce0f65bfe60bee6766ef6f6b8a5a0bd7f4
Ori Gold <nehoraigold@gmail.com>
2021-03-02 07:46:27 +0200

    Song: Added optional fields

--- src/model/Song.js
+++ src/model/Song.js
@@ -1,8 +1,11 @@
 class Song {
-    constructor(name, artist, videoId, durationSeconds = 0) {
+    constructor(name, artist, videoId, album = "", track = "", yearReleased = "", durationSeconds = 0) {
         this.name = name;
         this.artist = artist;
         this.youtubeVideoId = videoId;
+        this.album = album;
+        this.track = track;
+        this.yearReleased = yearReleased;
         this.durationSeconds = durationSeconds;
     }
 }
