b9f0d0e171e6a4d62a40ffe9846ee8b10f52dc24
Ori Gold <nehoraigold@gmail.com>
2021-03-19 22:33:52 +0200

    SongInfoSearcher: Added class to find song info

---
+++ src/song_info_searcher/SpotifySongInfoSearcher.js
@@ -0,0 +1,40 @@
+//region imports
+const SpotifyAPI = require("spotify-web-api-node");
+const { getYearFromDate } = require("../utils/utils");
+const Song = require("../model/Song");
+const ISongInfoSearcher = require("./ISongInfoSearcher");
+const SpotifyAuthorizer = require("../authorizer/SpotifyAuthorizer");
+const { SpotifyTrackField, parseSpotifyTrackFields, extractFieldsFromTrack } = require("../utils/spotify_utils");
+//endregion
+
+class SpotifySongInfoSearcher extends ISongInfoSearcher {
+    constructor(config) {
+        super();
+        this.spotify = new SpotifyAPI();
+        this.authorizer = new SpotifyAuthorizer(this.spotify, config.credentialsPath);
+    }
+
+    Initialize = async () => {
+        return await this.authorizer.Authorize();
+    };
+
+    Search = async (searchString) => {
+        const fields = parseSpotifyTrackFields(
+            SpotifyTrackField.TITLE,
+            SpotifyTrackField.ARTIST,
+            SpotifyTrackField.ALBUM,
+            SpotifyTrackField.TRACK_NUMBER,
+            SpotifyTrackField.ALBUM_TOTAL_TRACKS,
+            SpotifyTrackField.ALBUM_RELEASE_DATE
+        );
+        const { body: { tracks: { items } }} = await this.spotify.searchTracks(searchString, { fields });
+        if (items.length === 0) {
+            return null;
+        }
+        const songInfo = extractFieldsFromTrack(items[0]);
+        songInfo.yearReleased = getYearFromDate(songInfo.dateReleased);
+        return songInfo;
+    };
+}
+
+module.exports = SpotifySongInfoSearcher;
