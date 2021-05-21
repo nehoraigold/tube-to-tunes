550286c40a89d65b5e9b3b10a84d5dd78ee72798
Ori Gold <nehoraigold@gmail.com>
2021-05-21 21:48:09 +0300

    song_reviewer: added interface, modes, and PromptSongReviewer

---
+++ src/song_reviewer/PromptSongReviewer.js
@@ -0,0 +1,103 @@
+//region imports
+const chalk = require("chalk");
+const prompts = require("prompts");
+const ISongReviewer = require("./ISongReviewer");
+const ReviewMode = require("./ReviewMode");
+const { capitalize, splitCamelCase } = require("../utils/utils");
+//endregion
+
+class PromptSongReviewer extends ISongReviewer {
+    constructor(config) {
+        super();
+        this.enableSongSkip = config.enableSongSkip;
+        this.reviewFunction = this.getReviewFunction(config.reviewMode);
+    }
+
+    getReviewFunction = (reviewMode) => {
+        switch (reviewMode) {
+        case ReviewMode.DISPLAY:
+            return this.displaySong;
+        case ReviewMode.CONFIRM:
+            return this.confirmSong;
+        case ReviewMode.EDIT:
+            return this.editSong;
+        case ReviewMode.OFF:
+        default:
+            return null;
+        }
+    };
+
+    Review = async (songs) => {
+        const reviewedSongs = [];
+        for (let i = 0; i < songs.length; i++) {
+            const song = songs[i];
+            console.log(this.getSongHeader(song, i));
+            if (this.enableSongSkip && await this.shouldSkip()) {
+                continue;
+            }
+            const reviewedSong = this.reviewFunction ? await this.reviewFunction(song) : song;
+            if (reviewedSong) {
+                reviewedSongs.push(reviewedSong);
+            }
+        }
+        return reviewedSongs;
+    };
+
+    shouldSkip = async () => {
+        const { shouldDownload } = await prompts({
+            type: "confirm",
+            message: "Download this song?",
+            name: "shouldDownload",
+            initial: true
+        });
+        return !shouldDownload;
+    };
+
+    displaySong = async (song) => {
+        console.log(this.getSongDetails(song));
+        return song;
+    };
+
+    confirmSong = async (song) => {
+        console.log(this.getSongDetails(song));
+        const { songInfoIsCorrect } = await prompts([
+            {
+                type: "confirm",
+                message: "Are the song details correct?",
+                name: "songInfoIsCorrect",
+                initial: true
+            }]);
+        return songInfoIsCorrect ? song : await this.editSong(song);
+    };
+
+    editSong = async (song) => {
+        const questions = Object.entries(song).map(([prop, value]) => {
+            return {
+                type: value == null || prop === "durationSeconds" ? null : "text",
+                message: splitCamelCase(capitalize(prop)),
+                name: prop,
+                initial: value
+            };
+        });
+
+        return await prompts(questions);
+    };
+
+    getSongHeader = (song, index) => {
+        return chalk.bold.greenBright(`Song #${index + 1}`) +
+            `: '${chalk.yellowBright(song.name)}' by ${chalk.magentaBright(song.artist)}`;
+    };
+
+    getSongDetails = (song) => {
+        const properties = [];
+        for (const [property, value] of Object.entries(song)) {
+            if (property === "durationSeconds") {
+                continue;
+            }
+            properties.push(`  ${chalk.cyanBright(splitCamelCase(capitalize(property)))}: ${chalk.white(value)}`);
+        }
+        return properties.join("\n");
+    };
+}
+
+module.exports = PromptSongReviewer;
